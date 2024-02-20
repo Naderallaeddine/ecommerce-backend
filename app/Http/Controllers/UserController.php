06f43670f0fd9e418afe698de94f8d2ce17533e9
Naderallaeddine <naderallaeddine1@gmail.com>
2024-02-20 16:39:36 +0200

    add user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+    function register(Request $request){
+        $user=new User;
+        $user->name=$request->input('name');
+        $user->email=$request->input('email');
+        $user->password=Hash::make($request->input('password'));
+        $user->save();
+        return $user;
+
+    }
 }
