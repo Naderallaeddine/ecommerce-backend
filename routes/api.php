00c41f18a1ce1d357ad9e4969e854fe574fb4371
Naderallaeddine <naderallaeddine1@gmail.com>
2024-02-27 19:42:31 +0200

    add product

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +23,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('register',[UserController::class,'register']);
 Route::post('login',[UserController::class,'login']);
+Route::post('addproduct',[ProductController::class,'addProduct']);
